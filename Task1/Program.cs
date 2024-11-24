3d9bf76ec119c0b0c04a5d159975bcd1a9cd57e3
mrale <mrale@DESKTOP-HHS76V9>
2024-11-24 22:43:01 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static double SolutionGeron(double a, double b, double c)
+        {
+        double p = (a + b + c) / 2;
+        double s = Math.Sqrt (p * (p - a) * (p - b) * (p - c));
+        return s;
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите стороны первого треугольника");
+            double a1 = Convert.ToDouble(Console.ReadLine());
+            double b1 = Convert.ToDouble(Console.ReadLine());
+            double c1 = Convert.ToDouble(Console.ReadLine());
+            double s1 = SolutionGeron(a1, b1, c1);
+            Console.WriteLine("Введите стороны второго треугольника");
+            double a2 = Convert.ToDouble(Console.ReadLine());
+            double b2 = Convert.ToDouble(Console.ReadLine());
+            double c2 = Convert.ToDouble(Console.ReadLine());
+            double s2 = SolutionGeron(a2, b2, c2);
+
+            if (s1 > s2)
+
+                Console.WriteLine("Площадь первого треугольника больше");
+
+            else
+                if (s1 < s2)
+
+                Console.WriteLine("Площадь второго треугольника больше");
+
+            else
+
+                Console.WriteLine("Площади треугольников равны");
+
+            Console.ReadKey();
+
+            
+            
+
+
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void CalcCub (double h, out double s, out double v)
+        {
+            s = 6 * (h * h);
+            v = h * h * h;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину ребра");
+            double h = Convert.ToDouble(Console.ReadLine());
+            double s;
+            double v;
+            CalcCub(h, out s, out v);
+            Console.WriteLine("{0} {1}",s,v);
+            Console.ReadKey();
+        }
+    }
+}
